4eb99bac9296b1a04da2d1af0a935f57e592af89
qiekn <qiekndev@gmail.com>
2025-12-30 18:46:39 +0800

    script: lifesaver

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ TARGET="main"
 if [ "$1" = "debug" ]; then
   ${DEBUGGER} ./build/${TARGET}
 else
-  cmake --build ${BUILD} -j$(nproc) && ./${BUILD}/${TARGET}
+  cmake --build ${BUILD} -j$(nproc) && ./${BUILD}/${TARGET} > image.ppm && magick image.ppm image.jpg
 fi
 
 # vim: ft=sh ts=2 sw=2 et
