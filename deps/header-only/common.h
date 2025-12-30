c9ee75d4d2070d1984973693b8448da5400358d9
qiekn <qiekndev@gmail.com>
2025-12-30 23:06:41 +0800

    feat: rand utility func

--- deps/header-only/common.h
+++ deps/header-only/common.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 // C++ Std Usings
 using std::make_shared;
@@ -14,4 +16,20 @@ const double kPi = 3.1415926535897932385;
 // Utility Functions
 inline double Deg2Rad(double degress) {
   return degress * kPi / 180.0;
+}
+
+// Returns a random real in [0, 1)
+inline double RandomDouble() {
+  // This is the old wary
+  // return std::rand() / (RAND_MAX + 1.0);
+
+  // Now we can
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+
+// Returns a random real in [min, max)
+inline double RandomDouble(double min, double max) {
+  return min + (max - min) * RandomDouble();
 }
