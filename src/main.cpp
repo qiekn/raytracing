3a199ae73569c8b22490a8db5cda5020fc27216f
qiekn <qiekndev@gmail.com>
2025-12-30 17:20:58 +0800

    feat: add a progress indicator

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ostream>
 
 int main() {
 
@@ -10,6 +11,7 @@ int main() {
   std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanline remaining: " << (image_height - j) << ' ' << std::flush;
     for (int i = 0; i < image_width; i++) {
       auto r = double(i) / (image_width - 1);
       auto g = double(j) / (image_height - 1);
@@ -22,4 +24,5 @@ int main() {
       std::cout << ir << ' ' << ig << ' ' << ib << '\n';
     }
   }
+  std::clog << "\rDone.                 \n";
 }
