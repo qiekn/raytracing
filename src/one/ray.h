8a2611099c9602e7708282eec3bf0f53eb189a8f
qiekn <qiekndev@gmail.com>
2025-12-30 17:47:49 +0800

    feat: the ray class

---
+++ src/one/ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "vec3.h"
+
+class Ray {
+public:
+  Ray() {}
+
+  Ray(const point3& origin, const vec3& direction) : orig_(origin), dir_(direction) {}
+
+  const point3& origin() const { return orig_; }
+  const vec3& direction() const { return dir_; }
+
+  point3 at(double t) const {
+    return orig_ + t * dir_;
+  }
+
+private:
+  point3 orig_;
+  vec3 dir_;
+};
