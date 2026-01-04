fac6416c584edf9176fdbe570f8060e43b2178dc
qiekn <qiekndev@gmail.com>
2026-01-04 15:56:02 +0800

    feat: final scene of 'the next week'

--- src/main.cpp
+++ src/main.cpp
@@ -346,10 +346,84 @@ void CornellSmoke() {
   cam.Render(world);
 }
 
+void TheNextWeekFinalScene(int image_width, int samples_per_pixel, int max_depth) {
+  HittableList boxes1;
+  auto ground = make_shared<Lambertian>(color(0.48, 0.83, 0.53));
+
+  int boxes_per_side = 20;
+  for (int i = 0; i < boxes_per_side; i++) {
+    for (int j = 0; j < boxes_per_side; j++) {
+      auto w = 100.0;
+      auto x0 = -1000.0 + i * w;
+      auto z0 = -1000.0 + j * w;
+      auto y0 = 0.0;
+      auto x1 = x0 + w;
+      auto y1 = RandomDouble(1, 101);
+      auto z1 = z0 + w;
+
+      boxes1.Add(box(point3(x0, y0, z0), point3(x1, y1, z1), ground));
+    }
+  }
+
+  HittableList world;
+
+  world.Add(make_shared<BvhNode>(boxes1));
+
+  auto light = make_shared<DiffuseLight>(color(7, 7, 7));
+  world.Add(make_shared<Quad>(point3(123, 554, 147), vec3(300, 0, 0), vec3(0, 0, 265), light));
+
+  auto center1 = point3(400, 400, 200);
+  auto center2 = center1 + vec3(30, 0, 0);
+  auto sphere_material = make_shared<Lambertian>(color(0.7, 0.3, 0.1));
+  world.Add(make_shared<Sphere>(center1, center2, 50, sphere_material));
+
+  world.Add(make_shared<Sphere>(point3(260, 150, 45), 50, make_shared<Dielectric>(1.5)));
+  world.Add(make_shared<Sphere>(point3(0, 150, 145), 50,
+                                make_shared<Metal>(color(0.8, 0.8, 0.9), 1.0)));
+
+  auto boundary = make_shared<Sphere>(point3(360, 150, 145), 70, make_shared<Dielectric>(1.5));
+  world.Add(boundary);
+  world.Add(make_shared<ConstantMedium>(boundary, 0.2, color(0.2, 0.4, 0.9)));
+  boundary = make_shared<Sphere>(point3(0, 0, 0), 5000, make_shared<Dielectric>(1.5));
+  world.Add(make_shared<ConstantMedium>(boundary, .0001, color(1, 1, 1)));
+
+  auto emat = make_shared<Lambertian>(make_shared<ImageTexture>("earthmap.jpg"));
+  world.Add(make_shared<Sphere>(point3(400, 200, 400), 100, emat));
+  auto pertext = make_shared<NoiseTexture>(0.2);
+  world.Add(make_shared<Sphere>(point3(220, 280, 300), 80, make_shared<Lambertian>(pertext)));
+
+  HittableList boxes2;
+  auto white = make_shared<Lambertian>(color(.73, .73, .73));
+  int ns = 1000;
+  for (int j = 0; j < ns; j++) {
+    boxes2.Add(make_shared<Sphere>(point3::random(0, 165), 10, white));
+  }
+
+  world.Add(make_shared<Translate>(make_shared<RotateY>(make_shared<BvhNode>(boxes2), 15),
+                                   vec3(-100, 270, 395)));
+
+  Camera cam;
+
+  cam.aspect_ratio = 1.0;
+  cam.image_width = image_width;
+  cam.samples_per_pixel = samples_per_pixel;
+  cam.max_depth = max_depth;
+  cam.background = color(0, 0, 0);
+
+  cam.vfov = 40;
+  cam.lookfrom = point3(478, 278, -600);
+  cam.lookat = point3(278, 278, 0);
+  cam.vup = vec3(0, 1, 0);
+
+  cam.defocus_angle = 0;
+
+  cam.Render(world);
+}
+
 // ----------------------------------------------------------------------------: main
 int main() {
   // clang-format off
-  switch (9) {
+  switch (11) {
     // case 1
     // BVH on:  Render time: 52.8369s
     // BVH off: Render time: 236.774s
@@ -362,6 +436,8 @@ int main() {
     case 7: SimpleLights(); break;
     case 8: CornelBox(); break; // Render time: 311.929s
     case 9: CornellSmoke(); break;
+    case 10: TheNextWeekFinalScene(800, 10000, 40); break; // sweet dreams
+    case 11: TheNextWeekFinalScene(400, 250, 4); break;
   }
   // clang-format on
 }
